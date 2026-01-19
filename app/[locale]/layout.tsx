554fdd6407f098dd4d5c723162b3418594d1497a
xgraupera <grauperaxaviripoll@gmail.com>
2026-01-19 20:50:40 +0100

    V1.2

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "../globals.css";
 import type { ReactNode } from "react";
 import FooterBar from "@/components/FooterBar";
 import NavBar from "@/components/NavBar";
